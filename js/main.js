7a051b9f0b21c0e902a6d0179674d9d07121f517
mohammednasser249 <113276393+mohammednasser249@users.noreply.github.com>
2023-12-26 18:55:34 +0400

    add footer and make the page responsive

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function Open()
+{
+    var menu = document.getElementById("mobile-menu");
+    if(menu.style.display=="none")
+        menu.style.display="block";
+    else
+    menu.style.display="none";
+}
